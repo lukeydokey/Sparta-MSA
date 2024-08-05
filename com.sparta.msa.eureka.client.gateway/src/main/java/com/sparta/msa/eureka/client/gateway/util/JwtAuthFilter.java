f189b2893de8027217002eba3e0f63257acc8994
송형근 <soo1095@naver.com>
2024-08-05 18:52:51 +0900

    Gateway 로깅 필터 & JWT 필터 적용

---
+++ com.sparta.msa.eureka.client.gateway/src/main/java/com/sparta/msa/eureka/client/gateway/util/JwtAuthFilter.java
@@ -0,0 +1,72 @@
+package com.sparta.msa.eureka.client.gateway.util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+@Component
+@Slf4j
+public class JwtAuthFilter implements GlobalFilter {
+
+    @Value("${service.jwt.secret-key}")
+    private String secretKey;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+        if(path.equals("/api/auth/signIn") || path.equals("/api/auth/signUp")) {
+            return chain.filter(exchange);  // 로그인 회원가입 경로 필터를 적용 X
+        }
+
+        String token = extractToken(exchange);
+
+        if(token == null || !validateToken(token, exchange)){
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    private String extractToken(ServerWebExchange exchange) {
+        String header = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if(header != null && header.startsWith("Bearer ")){
+            return header.substring(7);
+        }
+        return null;
+    }
+
+    private boolean validateToken(String token, ServerWebExchange exchange) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .verifyWith(key)
+                    .build().parseSignedClaims(token);
+            log.info("payload : " + claimsJws.getPayload().toString());
+            exchange.getRequest().mutate()
+                    .header("X-User-Id", claimsJws.getPayload().get("USER_ID").toString())
+                    .header("X-User-Role", claimsJws.getPayload().get("USER_ROLE").toString())
+                    .build();
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+        return false;
+    }
+}
